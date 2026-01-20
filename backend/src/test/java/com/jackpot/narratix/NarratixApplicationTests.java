e7c3ddca21dbde09e9020b9e1fb3d0ae1754b900
kseysh <kseysh123@naver.com>
2026-01-20 17:13:15 +0900

    [init] 프로젝트 초기 세팅 (#6)

---
+++ backend/src/test/java/com/jackpot/narratix/NarratixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackpot.narratix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarratixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
