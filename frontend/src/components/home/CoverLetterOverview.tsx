5aa6db559fe24a5950ce9c525bfdb715da8891f5
apple <apple@appleui-MacBookPro.local>
2026-01-29 15:56:56 +0900

    [feat] 작성 중인 자기소개서 Overview 컴포넌트 추가
    - CoverLetterOverview: 작성 중인 자소서 목록 컨테이너
    
    - CoverLetterPreview: 파일 아이콘, 제목, 태그를 표시하는 프리뷰 카드
    
    - 3열 2행 그리드 레이아웃으로 구성

---
+++ frontend/src/components/home/CoverLetterOverview.tsx
@@ -0,0 +1,35 @@
+import CardIcon from './icons/CardIcon';
+import RightArrow from './icons/RightArrow';
+import CoverLetterPreview from './CoverLetterPreview';
+
+const CoverLetterOverview = () => {
+  return (
+    <div className='w-[82.5rem] inline-flex flex-col justify-start items-start gap-6'>
+      <div className='self-stretch inline-flex justify-between items-center'>
+        <div className='flex justify-start items-center gap-2.5'>
+          <div className='w-7 h-7 relative'>
+            <CardIcon />
+          </div>
+          <div className='justify-start text-gray-950 text-xl font-bold leading-9'>
+            작성 중인 자기소개서
+          </div>
+        </div>
+        <RightArrow size='lg' />
+      </div>
+      <div className='flex flex-col justify-start items-start gap-3'>
+        <div className='w-[82.5rem] inline-flex justify-start items-center gap-3'>
+          <CoverLetterPreview />
+          <CoverLetterPreview />
+          <CoverLetterPreview />
+        </div>
+        <div className='w-[82.5rem] inline-flex justify-start items-center gap-3'>
+          <CoverLetterPreview />
+          <CoverLetterPreview />
+          <CoverLetterPreview />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoverLetterOverview;
