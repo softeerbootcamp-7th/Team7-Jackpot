5a14a0c897fb51bfade8c5bc877169314d3ed055
kseysh <kseysh123@naver.com>
2026-01-30 21:45:52 +0900

    [feat] Review 엔티티 정의 (#261)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/Review.java
@@ -0,0 +1,33 @@
+package com.jackpot.narratix.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "review")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseTimeEntity {
+
+    @Id
+    private Long id;
+
+    @NotNull
+    @JoinColumn(name = "qna_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private QnA qna;
+
+    @NotNull
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "comment", nullable = true)
+    private String comment;
+
+    @Column(name = "suggest", nullable = true)
+    private String suggest;
+}
