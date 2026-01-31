98c55a118bdf026554c4c3913e1d65f423dcdb07
윤종근 <doodukly1999@gmail.com>
2026-02-01 02:19:00 +0900

    [refactor] 로그인 페이지 상수화 및 컴포넌트화

---
+++ frontend/src/constants/constatnsInLoginPage.ts
@@ -0,0 +1,14 @@
+export const INPUT_BAR_IN_LOGIN = [
+  {
+    ID: 'id',
+    TYPE: 'text',
+    PLACEHOLDER: '아이디를 입력해주세요',
+  },
+  {
+    ID: 'password',
+    TYPE: 'password',
+    PLACEHOLDER: '비밀번호를 입력해주세요',
+  },
+] as const;
+
+export const SUB_TITLE = '경험을 기록하는 가장 가벼운 시작' as const;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,24 +4,43 @@ import loginBackground from '/images/loginBackgroundImage.png';
 import titleLogo from '/images/titleLogo.svg';
 import { useNavigate } from 'react-router';
 
+import InputBar from '@/components/common/InputBar';
+
+import {
+  INPUT_BAR_IN_LOGIN,
+  SUB_TITLE,
+} from '@/constants/constatnsInLoginPage';
+
 import '@/index.css';
 
+interface FormDataType {
+  id: string;
+  password: string;
+}
+
 const LoginPage = () => {
-  const [idData, setIdData] = useState<string>('');
-  const [passwordData, setPasswordData] = useState<string>('');
+  const [formData, setFormData] = useState<FormDataType>({
+    id: '',
+    password: '',
+  });
   const navigate = useNavigate();
 
-  const isActived: boolean = idData !== '' && passwordData !== '';
+  const isActived: boolean = Object.values(formData).every(
+    (each) => each !== '',
+  );
+
   const buttonActiveStyle: string = isActived
     ? 'bg-gray-900 text-white cursor-pointer'
     : 'bg-gray-50 text-gray-400';
 
-  const saveUserId = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIdData(e.target.value);
-  };
-
-  const saveUserPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPasswordData(e.target.value);
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    key: keyof FormDataType,
+  ) => {
+    setFormData((prev) => ({
+      ...prev,
+      [key]: e.target.value,
+    }));
   };
 
   return (
@@ -35,24 +54,20 @@ const LoginPage = () => {
         <div className='flex flex-col items-center gap-3'>
           <img src={titleLogo} aria-label='타이틀 로고' />
           <div className='text-center justify-start text-gray-600 text-lg font-bold line-clamp-1 select-none'>
-            경험을 기록하는 가장 가벼운 시작
+            {SUB_TITLE}
           </div>
         </div>
         <div>
           <form className='flex flex-col justify-center items-center gap-6'>
             <div className='w-[392px] flex flex-col justify-center items-center gap-3'>
-              <input
-                className='w-full bg-gray-50 px-5 py-[14px] rounded-lg'
-                type='text'
-                placeholder='아이디를 입력해주세요'
-                onChange={saveUserId}
-              />
-              <input
-                className='w-full bg-gray-50 px-5 py-[14px] rounded-lg'
-                type='password'
-                placeholder='비밀번호를 입력해주세요'
-                onChange={saveUserPassword}
-              />
+              {INPUT_BAR_IN_LOGIN.map((each) => (
+                <InputBar
+                  key={each.ID}
+                  type={each.TYPE}
+                  placeholder={each.PLACEHOLDER}
+                  onChange={(e) => handleInputChange(e, each.ID)}
+                />
+              ))}
             </div>
             <input
               className={`w-full ${buttonActiveStyle} px-5 py-[12px] rounded-lg`}
@@ -62,7 +77,10 @@ const LoginPage = () => {
             />
           </form>
         </div>
-        <button className='text-gray-600 font-medium text-base cursor-pointer' onClick={() => navigate('/signup')}>
+        <button
+          className='text-gray-600 font-medium text-base cursor-pointer'
+          onClick={() => navigate('/signup')}
+        >
           회원가입
         </button>
       </div>
