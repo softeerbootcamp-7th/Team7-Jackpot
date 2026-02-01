5f7703eb30e4c07f87195c349eb3d3119036fcec
윤종근 <doodukly1999@gmail.com>
2026-02-01 10:39:25 +0900

    [feat] 회원가입 시 아이디 중복확인 컴포넌트 생성

---
+++ frontend/src/components/signUp/CheckDuplicationButton.tsx
@@ -0,0 +1,23 @@
+interface CheckDuplicationButton {
+  isActived: boolean;
+}
+
+const CheckDuplicationButton = ({ isActived }: CheckDuplicationButton) => {
+  const buttonActiveStyle: string = isActived
+    ? 'bg-gray-900 text-white cursor-pointer'
+    : 'bg-gray-100 text-gray-400';
+
+  return (
+    <button
+      type='button'
+      disabled={!isActived}
+      className={`px-3 py-1.5 rounded-md text-sm font-bold transition-colors ${
+        buttonActiveStyle
+      }`}
+    >
+      중복확인
+    </button>
+  );
+};
+
+export default CheckDuplicationButton;
