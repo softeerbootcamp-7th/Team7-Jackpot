e96bd14db8fc23a411cde4f0aaa930ff9a4e0d35
윤종근 <doodukly1999@gmail.com>
2026-02-01 13:11:59 +0900

    [feat] 회원가입 입력 필드 유효성 검사 로직 추가

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import loginBackground from '/images/loginBackgroundImage.png';
 
@@ -12,6 +12,11 @@ import {
   INPUT_BAR_IN_SIGNUP,
   SUB_TITLE,
 } from '@/constants/constantsInSignUpPage';
+import {
+  validateId,
+  validateNickname,
+  validatePassword,
+} from '@/utils/signUp/validation';
 
 import '@/index.css';
 
@@ -26,6 +31,16 @@ interface isActivedType {
   id: boolean;
   submit: boolean;
 }
+
+interface StatusMsgType {
+  id: string;
+  password: string;
+  passwordCheck: string;
+  nickname: string;
+}
+
+type InputIdType = (typeof INPUT_BAR_IN_SIGNUP)[number]['ID'];
+
 const SignUpPage = () => {
   const [formData, setFormData] = useState<FormDataType>({
     id: '',
@@ -34,16 +49,102 @@ const SignUpPage = () => {
     nickname: '',
   });
 
-  const isActived: isActivedType = {
-    id: formData.id.length >= 6,
-    submit: Object.values(formData).every((each) => each !== ''),
-  };
+  const [statusMsg, setStatusMsg] = useState<StatusMsgType>({
+    id: '',
+    password: '',
+    passwordCheck: '',
+    nickname: '',
+  });
+
+  const [isPasswordMatched, setIsPasswordMatched] = useState<boolean>(false);
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement>,
-    key: keyof FormDataType,
+    key: InputIdType,
   ) => {
-    setFormData((prev) => ({ ...prev, [key]: e.target.value }));
+    let value = e.target.value;
+
+    switch (key) {
+      case 'id':
+        value = value.toLowerCase().replace(/[^a-z0-9]/g, '');
+        break;
+      case 'password':
+      case 'passwordCheck':
+        value = value.replace(/\s/g, '');
+        break;
+      case 'nickname':
+        value = value.replace(/[0-9!@#$%^&*()_+={}[\]:;"'<>,.?/\\|`~\s]/g, '');
+        break;
+      default:
+        break;
+    }
+
+    setFormData((prev) => ({ ...prev, [key]: value }));
+  };
+
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      const newMsg = { ...statusMsg };
+      let isMatch = false;
+
+      if (formData.id) {
+        if (!validateId(formData.id)) {
+          newMsg.id = '6~12자의 영문 소문자, 숫자만 사용 가능합니다.';
+        } else {
+          newMsg.id = '';
+        }
+      } else {
+        newMsg.id = '';
+      }
+
+      if (formData.password) {
+        newMsg.password = validatePassword(formData.password)
+          ? ''
+          : '비밀번호 형식이 올바르지 않습니다. (영문, 숫자 조합 8자 이상)';
+      } else {
+        newMsg.password = '';
+      }
+
+      if (formData.passwordCheck) {
+        isMatch = formData.password === formData.passwordCheck;
+
+        newMsg.passwordCheck = isMatch
+          ? '비밀번호가 일치합니다.'
+          : '비밀번호가 일치하지 않습니다.';
+      } else {
+        newMsg.passwordCheck = '';
+        isMatch = false;
+      }
+
+      const name = formData.nickname;
+      if (name) {
+        if (name.length < 2) {
+          newMsg.nickname = '2자 이상 입력해주세요';
+        } else if (!validateNickname(name)) {
+          newMsg.nickname = '형식이 올바르지 않습니다';
+        } else {
+          newMsg.nickname = '';
+        }
+      } else {
+        newMsg.nickname = '';
+      }
+
+      setStatusMsg(newMsg);
+      setIsPasswordMatched(isMatch);
+    }, 1000);
+
+    return () => clearTimeout(debounceTimer);
+  }, [formData, statusMsg]);
+
+  const isActived: isActivedType = {
+    id: validateId(formData.id),
+
+    submit:
+      validateId(formData.id) &&
+      validatePassword(formData.password) &&
+      formData.password === formData.passwordCheck &&
+      formData.nickname.length >= 2 &&
+      validateNickname(formData.nickname),
   };
 
   return (
@@ -61,20 +162,31 @@ const SignUpPage = () => {
         />
         <form className='flex flex-col justify-center items-center gap-[3.75rem]'>
           <div className='w-[24.5rem] flex flex-col justify-center items-center gap-[1.125rem]'>
-            {INPUT_BAR_IN_SIGNUP.map((each) => (
-              <InputBarInSignUp
-                key={each.ID}
-                hintText={each.HINT_TEXT}
-                type={each.TYPE}
-                placeholder={each.PLACEHOLDER}
-                onChange={(e) => handleInputChange(e, each.ID)}
-                rightElement={
-                  each.ID === 'id' && (
-                    <CheckDuplicationButton isActived={isActived.id} />
-                  )
-                }
-              />
-            ))}
+            {INPUT_BAR_IN_SIGNUP.map((each) => {
+              const currentMsg = statusMsg[each.ID];
+
+              const isPasswordMatchSuccess =
+                each.ID === 'passwordCheck' && isPasswordMatched;
+
+              return (
+                <InputBarInSignUp
+                  key={each.ID}
+                  hintText={each.HINT_TEXT}
+                  type={each.TYPE}
+                  placeholder={each.PLACEHOLDER}
+                  maxLength={each.MAX_LENGTH}
+                  onChange={(e) => handleInputChange(e, each.ID)}
+                  value={formData[each.ID]}
+                  helpMessage={currentMsg}
+                  isSuccess={isPasswordMatchSuccess}
+                  rightElement={
+                    each.ID === 'id' && (
+                      <CheckDuplicationButton isActived={isActived.id} />
+                    )
+                  }
+                />
+              );
+            })}
           </div>
           <SubmitButton isActived={isActived.submit} value='회원가입' />
         </form>
