ba849c03b9982f8efc7f11fda43296929d4506bd
윤종근 <doodukly1999@gmail.com>
2026-02-01 10:40:24 +0900

    [feat] 입력바에 조건부 렌더링 로직 추가

--- frontend/src/components/common/InputBar.tsx
+++ frontend/src/components/common/InputBar.tsx
@@ -2,20 +2,29 @@ export interface InputBarProps {
   type: string;
   placeholder: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  rightElement?: React.ReactNode;
 }
 
 const InputBar = ({
   type,
   placeholder,
   onChange,
+  rightElement,
 }: InputBarProps) => {
   return (
-    <input
-      className='w-full bg-gray-50 px-5 py-[0.875rem] rounded-lg'
-      type={type}
-      placeholder={placeholder}
-      onChange={onChange}
-    />
+    <div className='relative w-full'>
+      <input
+        className='w-full bg-gray-50 px-5 py-[0.875rem] rounded-lg'
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+      {rightElement && (
+        <div className='absolute right-[0.75rem] top-1/2 -translate-y-1/2'>
+          {rightElement}
+        </div>
+      )}
+    </div>
   );
 };
 
