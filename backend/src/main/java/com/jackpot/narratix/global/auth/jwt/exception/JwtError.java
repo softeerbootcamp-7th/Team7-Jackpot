021e5ebfc0b55126906ad24524c15975ea41f204
kseysh <kseysh123@naver.com>
2026-02-02 11:07:14 +0900

    [feat] Jwt 전용 에러처리 구 (#264)

--- backend/src/main/java/com/jackpot/narratix/global/auth/jwt/exception/JwtError.java
+++ backend/src/main/java/com/jackpot/narratix/global/auth/jwt/exception/JwtError.java
@@ -1,20 +1,19 @@
 package com.jackpot.narratix.global.auth.jwt.exception;
 
+import com.jackpot.narratix.global.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum JwtError {
+public enum JwtError implements ErrorCode {
 
     // 401 UNAUTHORIZED
-    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다. 액세스 토큰을 재발급 받아주세요."),
-    EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED,  "액세스 토큰이 만료되었습니다. 액세스 토큰을 재발급 받아주세요."),
-
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다. 다시 로그인 해주세요."),
-    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED,  "리프레시 토큰이 만료되었습니다. 다시 로그인해 주세요.");
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다. 토큰을 재발급 받아주세요."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다. 다시 로그인해 주세요."),
+    MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰 형식이 잘못되었습니다.");
 
     private final HttpStatus status;
-    private final String errorMessage;
+    private final String message;
 }
