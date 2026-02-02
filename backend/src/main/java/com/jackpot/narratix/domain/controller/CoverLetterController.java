6786511ab1286f3b90498659d506bc65d7c06b64
kseysh <kseysh123@naver.com>
2026-02-03 08:42:39 +0900

    [feat] 자기소개서 삭제 컨트롤러 로직 추가 (#238)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/CoverLetterController.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/CoverLetterController.java
@@ -5,12 +5,11 @@ import com.jackpot.narratix.domain.controller.response.CreateCoverLetterResponse
 import com.jackpot.narratix.domain.service.CoverLetterService;
 import com.jackpot.narratix.global.auth.UserId;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +24,14 @@ public class CoverLetterController {
             @RequestBody @Valid CreateCoverLetterRequest createCoverLetterRequest
     ) {
         return ResponseEntity.ok(coverLetterService.createNewCoverLetter(userId, createCoverLetterRequest));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteCoverLetterById(
+            @UserId String userId,
+            @RequestParam @Validated @NotNull Long coverLetterId
+    ) {
+        coverLetterService.deleteCoverLetterById(userId, coverLetterId);
+        return ResponseEntity.noContent().build();
     }
 }
