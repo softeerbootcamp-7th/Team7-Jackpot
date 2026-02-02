eab31c3ea3cdd3c7cf5e83b1b25dd24965ade745
kseysh <kseysh123@naver.com>
2026-02-03 08:43:02 +0900

    [feat] 자기소개서 삭제 레포지토리 로직 추가 (#238)

--- backend/src/main/java/com/jackpot/narratix/domain/repository/CoverLetterRepository.java
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/CoverLetterRepository.java
@@ -1,10 +1,17 @@
 package com.jackpot.narratix.domain.repository;
 
 import com.jackpot.narratix.domain.entity.CoverLetter;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CoverLetterRepository {
 
     CoverLetter save(CoverLetter coverLetter);
+
+    Optional<CoverLetter> findById(@NotNull Long coverLetterId);
+
+    void deleteById(@NotNull Long coverLetterId);
 }
--- backend/src/main/java/com/jackpot/narratix/domain/repository/CoverLetterRepositoryImpl.java
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/CoverLetterRepositoryImpl.java
@@ -4,6 +4,8 @@ import com.jackpot.narratix.domain.entity.CoverLetter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class CoverLetterRepositoryImpl implements CoverLetterRepository{
@@ -13,5 +15,15 @@ public class CoverLetterRepositoryImpl implements CoverLetterRepository{
     @Override
     public CoverLetter save(CoverLetter coverLetter) {
         return coverLetterJpaRepository.save(coverLetter);
+    }
+
+    @Override
+    public Optional<CoverLetter> findById(Long coverLetterId) {
+        return coverLetterJpaRepository.findById(coverLetterId);
+    }
+
+    @Override
+    public void deleteById(Long coverLetterId) {
+        coverLetterJpaRepository.deleteById(coverLetterId);
     }
 }
