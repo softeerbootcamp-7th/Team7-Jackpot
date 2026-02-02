cb068202b3b1a91f90864a912de9e74dd6155a5b
kseysh <kseysh123@naver.com>
2026-02-02 16:33:59 +0900

    [chore] CreateCoverLetterRequest 포매팅 진행 (#236)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/request/CreateCoverLetterRequest.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/request/CreateCoverLetterRequest.java
@@ -7,10 +7,12 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
 
-public record CreateCoverLetterRequest(@NotNull(message = "기업명은 필수 입력 항목입니다.") String companyName,
-                                       @NotNull(message = "채용년도는 필수 입력 항목입니다.") Integer applyYear,
-                                       @NotNull(message = "채용분기는 필수 입력 항목입니다.") ApplyHalfType applyHalf,
-                                       @NotNull(message = "직무명은 필수 입력 항목입니다.") String jobPosition, LocalDate deadline,
-                                       @NotNull(message = "질문은 필수 입력 항목입니다.") @Size(min = 1, max = 3, message = "질문은 최소 1개에서 최대 3개까지 입력할 수 있습니다.") List<CreateQuestionRequest> questions) {
-
+public record CreateCoverLetterRequest(
+        @NotNull(message = "기업명은 필수 입력 항목입니다.") String companyName,
+        @NotNull(message = "채용년도는 필수 입력 항목입니다.") Integer applyYear,
+        @NotNull(message = "채용분기는 필수 입력 항목입니다.") ApplyHalfType applyHalf,
+        @NotNull(message = "직무명은 필수 입력 항목입니다.") String jobPosition, LocalDate deadline,
+        @NotNull(message = "질문은 필수 입력 항목입니다.")
+        @Size(min = 1, max = 3, message = "질문은 최소 1개에서 최대 3개까지 입력할 수 있습니다.")
+        List<CreateQuestionRequest> questions) {
 }
