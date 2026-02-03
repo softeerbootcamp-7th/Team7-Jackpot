8005f23e841d791754384c7e1aa3da373a15ae81
livebylee <livebylee@gmail.com>
2026-02-03 14:18:55 +0900

    [feat] 기업 라이브러리 조회 service 로직 완성  (#230)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/LibraryController.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/LibraryController.java
@@ -3,27 +3,30 @@ package com.jackpot.narratix.domain.controller;
 import com.jackpot.narratix.domain.controller.request.GetLibraryListRequest;
 import com.jackpot.narratix.domain.controller.response.GetLibraryListResponse;
 import com.jackpot.narratix.domain.service.LibraryService;
+import com.jackpot.narratix.global.auth.UserId;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/library")
 public class LibraryController {
 
-    LibraryService libraryService;
+    private final LibraryService libraryService;
 
     @GetMapping("/all")
     public ResponseEntity<GetLibraryListResponse> getLibraryList(
+            @UserId String userId,
             @Valid @ModelAttribute GetLibraryListRequest request
     ) {
-        List<String> names = libraryService.getLibraryList(request);
-        return ResponseEntity.ok(new GetLibraryListResponse(names));
+        List<String> libraryList = libraryService.getLibraryList(userId, request);
+        return ResponseEntity.ok(new GetLibraryListResponse(libraryList));
     }
 }
--- backend/src/main/java/com/jackpot/narratix/domain/service/LibraryService.java
+++ backend/src/main/java/com/jackpot/narratix/domain/service/LibraryService.java
@@ -2,21 +2,28 @@ package com.jackpot.narratix.domain.service;
 
 import com.jackpot.narratix.domain.controller.request.GetLibraryListRequest;
 import com.jackpot.narratix.domain.entity.enums.QuestionCategoryType;
+import com.jackpot.narratix.domain.repository.CoverLetterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class LibraryService {
 
-    public List<String> getLibraryList(GetLibraryListRequest request) {
+    private final CoverLetterRepository coverLetterRepository;
+
+    public List<String> getLibraryList(String userId, GetLibraryListRequest request) {
         return switch (request.type()) {
-            case COMPANY -> getCompanyName();
+            case COMPANY -> getCompanyName(userId);
             case QUESTION -> getQuestionCategory();
         };
     }
 
-    private List<String> getCompanyName() {
-
+    private List<String> getCompanyName(String userId) {
+        return coverLetterRepository.findCompanyNamesByUserId(userId);
     }
 
     private List<String> getQuestionCategory() {
