eb78b33fddd5f79ef6aada2a2648b02ef74e6617
kseysh <kseysh123@naver.com>
2026-02-03 10:45:38 +0900

    [feat] ApplyHalfType 매핑 함수 구현 (#238)

--- backend/src/main/java/com/jackpot/narratix/domain/entity/enums/ApplyHalfType.java
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/enums/ApplyHalfType.java
@@ -1,5 +1,12 @@
 package com.jackpot.narratix.domain.entity.enums;
 
+import java.time.LocalDate;
+
 public enum ApplyHalfType {
-    FIRST_HALF, SECOND_HALF
+    FIRST_HALF, SECOND_HALF;
+
+    public static ApplyHalfType caculateApplyHalfType(LocalDate date) {
+        if (date.getMonthValue() <= 6) return FIRST_HALF;
+        return SECOND_HALF;
+    }
 }
