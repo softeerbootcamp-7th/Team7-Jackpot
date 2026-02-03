754bd0d5e11f6bfb1f1ddf6be2ea76f8c80de376
윤종근 <doodukly1999@gmail.com>
2026-02-03 20:28:09 +0900

    [feat] 콘텐츠 영역 헤더 단계 구분을 위한 파라미터 추가

--- frontend/src/types/upload.ts
+++ frontend/src/types/upload.ts
@@ -1,4 +1,6 @@
 export interface FirstContentAreaHeaderProps {
   uploadTab: 'file' | 'text';
   setUploadTab: (newValue: 'file' | 'text') => void;
+  step?: string;
+  nextStep?: (step: string) => void;
 }
