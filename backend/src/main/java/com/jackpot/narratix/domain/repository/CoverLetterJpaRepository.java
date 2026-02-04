a241a2c3d4c09a446a11e2fb184ef86a4254568d
livebylee <livebylee@gmail.com>
2026-02-04 10:54:20 +0900

    [refactor] @query파라미터에 @Param 지정  (#230)

--- backend/src/main/java/com/jackpot/narratix/domain/repository/CoverLetterJpaRepository.java
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/CoverLetterJpaRepository.java
@@ -4,6 +4,7 @@ import com.jackpot.narratix.domain.entity.CoverLetter;
 import com.jackpot.narratix.domain.entity.enums.ApplyHalfType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,5 +15,5 @@ public interface CoverLetterJpaRepository extends JpaRepository<CoverLetter, Lon
     Integer countByUserIdAndApplyYearAndApplyHalf(String userId, int applyYear, ApplyHalfType applyHalf);
 
     @Query("SELECT DISTINCT c.companyName FROM CoverLetter c WHERE c.userId = :userId")
-    List<String> findDistinctCompanyNamesByUserId(String userId);
+    List<String> findDistinctCompanyNamesByUserId(@Param("userId") String userId);
 }
--- backend/src/main/java/com/jackpot/narratix/domain/repository/QnAJpaRepository.java
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/QnAJpaRepository.java
@@ -4,6 +4,7 @@ import com.jackpot.narratix.domain.entity.QnA;
 import com.jackpot.narratix.domain.entity.enums.QuestionCategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,5 @@ public interface QnAJpaRepository extends JpaRepository<QnA, Long> {
     Integer countByUserId(String userId);
 
     @Query("SELECT DISTINCT q.questionCategory FROM QnA q WHERE q.userId = :userId")
-    List<QuestionCategoryType> findDistinctByQuestionCategory(String userId);
+    List<QuestionCategoryType> findDistinctByQuestionCategory(@Param("userId") String userId);
 }
