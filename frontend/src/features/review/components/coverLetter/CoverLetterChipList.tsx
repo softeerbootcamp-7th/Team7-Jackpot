5ee226156fb9dce2b35c8f5fa62d7b0e7bb74fcb
using6843@gmail.com <using6843@gmail.com>
2026-02-05 10:36:09 +0900

    [chore] 잘못된 디자인 토큰 값 수정

--- frontend/src/features/review/components/coverLetter/CoverLetterChipList.tsx
+++ frontend/src/features/review/components/coverLetter/CoverLetterChipList.tsx
@@ -5,21 +5,21 @@ interface CoverLetterChipListProps {
 
 const CoverLetterChipList = ({ company, job }: CoverLetterChipListProps) => {
   return (
-    <div className='shrink-0 h-[2.5rem] flex items-center'>
+    <div className='flex h-[2.5rem] shrink-0 items-center'>
       <div className='flex items-start gap-[0.25rem]'>
         <div
           data-속성-1='comp chip'
-          className='px-[0.75rem] py-[0.375rem] bg-blue-50 rounded-xl flex items-center gap-0.25'
+          className='flex items-center gap-[0.25rem] rounded-xl bg-blue-50 px-[0.75rem] py-[0.375rem]'
         >
-          <div className='text-blue-600 text-xs font-medium leading-4'>
+          <div className='text-xs leading-4 font-medium text-blue-600'>
             {company}
           </div>
         </div>
         <div
           data-속성-1='job chip'
-          className='px-[0.75rem] py-[0.375rem] bg-gray-50 rounded-xl flex items-center gap-0.25'
+          className='flex items-center gap-[0.25rem] rounded-xl bg-gray-50 px-[0.75rem] py-[0.375rem]'
         >
-          <div className='text-gray-600 text-xs font-medium leading-4'>
+          <div className='text-xs leading-4 font-medium text-gray-600'>
             {job}
           </div>
         </div>
