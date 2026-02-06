b77c94020bfc8524dc45237cb4b9d96b2a9a7a59
Sminp <0102262@gmail.com>
2026-02-06 13:46:42 +0900

    [feat] 자기소게서 작성 페이지를 구축하여 랜딩 페이지(자기소개서를 리스트로 볼 수 있는 페이지), 사이드바 및 새로운 자기소개서 등록 버튼 추가 (#199)

---
+++ frontend/src/pages/CoverLetterPage.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+import Button from '@/features/coverLetter/components/Button';
+import CoverLetterWriteSidebar from '@/features/coverLetter/components/CoverLetterWriteSidebar';
+import NewCoverLetter from '@/features/coverLetter/components/newCoverLetter/NewCoverLetter';
+import {
+  coverLetterHeaderText,
+  emptyCaseText,
+} from '@/features/coverLetter/constants';
+import ContentHeader from '@/shared/components/ContentHeader';
+import CoverLetterOverview from '@/shared/components/CoverLetterOverview';
+import DataGuard from '@/shared/components/DataGuard';
+import EmptyCase from '@/shared/components/EmptyCase';
+import SearchInput from '@/shared/components/SearchInput';
+import SidebarLayout from '@/shared/components/SidebarLayout';
+
+const CoverLetterPage = () => {
+  const [isLanding, setIsLanding] = useState(true);
+
+  const hasData = true;
+
+  const handleSearch = () => {};
+
+  // 나중에 페이지 분기처리 리팩토링
+  // isLanding으로 랜딩 페이지, 자기소개서 등록/수정 페이지 분기
+  // 자기소개서 등록/수정 페이지는 사이드바가 아닌 메인 콘텐츠로 분기
+
+  return (
+    <SidebarLayout
+      headerSlot={
+        <>
+          <ContentHeader {...coverLetterHeaderText} />
+          {isLanding && (
+            <div className='flex flex-row items-center justify-between'>
+              <SearchInput
+                onSearch={handleSearch}
+                placeholder='문항 유형을 입력해주세요'
+              />
+              <Button handleClick={setIsLanding} />
+            </div>
+          )}
+        </>
+      }
+      sidebarSlot={!isLanding && <CoverLetterWriteSidebar />}
+    >
+      <DataGuard
+        data={hasData}
+        fallback={<EmptyCase {...emptyCaseText.overview} />}
+      >
+        {isLanding ? (
+          <CoverLetterOverview isCoverLetter={true} len={9} />
+        ) : (
+          <NewCoverLetter />
+        )}
+      </DataGuard>
+    </SidebarLayout>
+  );
+};
+
+export default CoverLetterPage;
