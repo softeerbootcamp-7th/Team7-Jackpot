956be7ed9a34cddedc6bf6eb96776b0f72e58b93
livebylee <livebylee@gmail.com>
2026-02-09 18:18:28 +0900

    [fix] 충돌 해결 중 에러 수정(#242)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/api/QnAApi.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/api/QnAApi.java
@@ -14,10 +14,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
