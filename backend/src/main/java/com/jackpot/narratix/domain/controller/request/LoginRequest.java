cb3060529060b87cd108430c5427fa938fd7e1e5
kseysh <kseysh123@naver.com>
2026-02-09 11:55:43 +0900

    [refactor] 로그인 시 NotNull Validation 추가 (#330)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/request/LoginRequest.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.jackpot.narratix.domain.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,9 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginRequest {
 
+    @NotNull(message = "아이디는 필수 입력 항목입니다.")
     @NotBlank(message = "아이디는 필수 입력 항목입니다.")
     private String userId;
 
+    @NotNull(message = "비밀번호는 필수 입력 항목입니다.")
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
     private String password;
 }
