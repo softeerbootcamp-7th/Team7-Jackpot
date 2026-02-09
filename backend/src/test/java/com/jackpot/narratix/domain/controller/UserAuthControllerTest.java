4236a1b4778864d0232cafc57314791718deb479
kseysh <kseysh123@naver.com>
2026-02-09 20:37:50 +0900

    [chore] 불필요한 어노테이션 제거 (#329)

--- backend/src/test/java/com/jackpot/narratix/domain/controller/UserAuthControllerTest.java
+++ backend/src/test/java/com/jackpot/narratix/domain/controller/UserAuthControllerTest.java
@@ -18,7 +18,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -33,7 +32,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
-@ActiveProfiles("test")
 class UserAuthControllerTest {
 
     @InjectMocks
