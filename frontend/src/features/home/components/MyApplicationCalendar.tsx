e4630a1de5a182794396b6dad6d64afc192dc8db
Sminp <0102262@gmail.com>
2026-02-10 09:37:07 +0900

    [feat] 3주 캘린더 훅을 만들어서 사용하려고 시도함 (그러나 이미 구현되었다고 해서 shared의 훅을 활용하여  리팩토링할 예정)

--- frontend/src/features/home/components/MyApplicationCalendar.tsx
+++ frontend/src/features/home/components/MyApplicationCalendar.tsx
@@ -1,7 +1,18 @@
+import MyApplicationCalendarDay from '@/features/home/components/MyApplicationCalendarDay';
+import { useMyApplicationCalendar } from '@/features/home/hooks/useMyApplicationCalendar';
 import CalendarIcon from '@/features/home/icons/CalendarIcon';
+import { weekList } from '@/shared/constants/dates';
 import RightArrow from '@/shared/icons/RightArrow';
+import type { Day } from '@/shared/types/date';
+
+const dayColorMap: Partial<Record<Day, string>> = {
+  일: 'text-rose-400',
+  토: 'text-blue-500',
+};
 
 const MyApplicationCalendar = () => {
+  const { days, isPastDay, hasApplication } = useMyApplicationCalendar();
+
   return (
     <div className='inline-flex w-96 flex-col items-start justify-start gap-6'>
       <div className='inline-flex items-center justify-between self-stretch'>
@@ -19,49 +30,30 @@ const MyApplicationCalendar = () => {
       </div>
       <div className='flex flex-col items-start justify-start gap-1 self-stretch'>
         <div className='inline-flex items-center justify-between self-stretch'>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-center rounded-[125px] px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-rose-400'>
-              일
+          {weekList.map((d) => (
+            <div
+              key={d}
+              className='inline-flex h-12 w-12 flex-col items-center justify-center rounded-[125px] px-4 py-1'
+            >
+              <div
+                className={`h-5 w-6 justify-center text-center text-xl leading-4 font-normal ${dayColorMap[d] || 'text-gray-950'}`}
+              >
+                {d}
+              </div>
             </div>
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-center rounded-[125px] px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              월
-            </div>
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-center rounded-[125px] px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              화
-            </div>
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-center rounded-[125px] px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              수
-            </div>
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-center rounded-[125px] px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              목
-            </div>
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-center rounded-[125px] px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              금
-            </div>
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-center rounded-[125px] px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-blue-500'>
-              토
-            </div>
-          </div>
+          ))}
         </div>
-        <div className='inline-flex items-center justify-between self-stretch'>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1 opacity-40'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950 opacity-40'>
-              1
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full bg-gray-950 opacity-40' />
-          </div>
+        {/* [박소민] TODO: 현재 전 날짜이면 회색, 현재 날짜이면 날짜가 동그라미, 마감일이 있다면 아래에 점표시 */}
+        <div className='grid grid-cols-7 items-center justify-between self-stretch'>
+          {days.map((d, idx) => (
+            <MyApplicationCalendarDay
+              key={idx}
+              date={d}
+              isPastDate={isPastDay(d)}
+              hasApplication={hasApplication()}
+            />
+          ))}
+
           <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1 opacity-40'>
             <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950 opacity-40'>
               2
@@ -89,100 +81,6 @@ const MyApplicationCalendar = () => {
           <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
             <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
               6
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              7
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full bg-purple-500' />
-          </div>
-        </div>
-        <div className='inline-flex items-center justify-between self-stretch'>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              8
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full bg-purple-500' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              9
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              10
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              11
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full bg-purple-500' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              12
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              13
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              14
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full bg-purple-500' />
-          </div>
-        </div>
-        <div className='inline-flex items-center justify-between self-stretch'>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              15
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full bg-purple-500' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              16
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              17
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              18
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              19
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              20
-            </div>
-            <div className='h-[5px] w-[5px] rounded-full bg-purple-500' />
-          </div>
-          <div className='inline-flex h-12 w-12 flex-col items-center justify-end gap-1.5 rounded-2xl px-4 py-1'>
-            <div className='h-5 w-6 justify-center text-center text-xl leading-4 font-normal text-gray-950'>
-              21
             </div>
             <div className='h-[5px] w-[5px] rounded-full' />
           </div>
