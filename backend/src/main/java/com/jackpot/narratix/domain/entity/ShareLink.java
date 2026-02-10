db2334c3a6ede1f6dede5f046796de2624a555b1
kseysh <kseysh123@naver.com>
2026-02-10 12:10:55 +0900

    [feat] ShareLink Entity 정의 (#339)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/ShareLink.java
@@ -0,0 +1,54 @@
+package com.jackpot.narratix.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "share_link")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ShareLink {
+
+    @Id
+    @Column(name = "cover_letter_id")
+    private Long coverLetterId;
+
+    @Column(name = "share_id", nullable = false)
+    private String shareId;
+
+    @NotNull
+    @Column(name = "is_shared", nullable = false)
+    private boolean isShared;
+
+    @NotNull
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    public static ShareLink newActivatedShareLink(Long coverLetterId){
+        return new ShareLink(
+                coverLetterId,
+                UUID.randomUUID().toString(),
+                true,
+                LocalDateTime.now().plusDays(14)
+        );
+    }
+
+    public void activate(){
+        this.isShared = true;
+        this.shareId = UUID.randomUUID().toString();
+        this.expiresAt = LocalDateTime.now().plusDays(14);
+    }
+
+    public void deactivate(){
+        this.isShared = false;
+    }
+
+}
