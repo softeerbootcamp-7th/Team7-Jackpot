f98d36ecb759d436056b3ba6d13f7f6b17f11d1d
kseysh <kseysh123@naver.com>
2026-02-10 12:15:29 +0900

    [feat] 첨삭 링크 활성화/비활성화 레포지토리 정의 (#339)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/ShareLinkRepository.java
@@ -0,0 +1,8 @@
+package com.jackpot.narratix.domain.repository;
+
+import com.jackpot.narratix.domain.entity.ShareLink;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShareLinkRepository extends JpaRepository<ShareLink, Long> {
+
+}
