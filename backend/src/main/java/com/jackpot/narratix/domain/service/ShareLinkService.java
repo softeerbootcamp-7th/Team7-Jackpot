087f70859a802b38280289677eef6f9007203eff
kseysh <kseysh123@naver.com>
2026-02-10 12:18:41 +0900

    [feat] 첨삭 링크 활성화/비활성화 서비스 로직 구현 (#339)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/service/ShareLinkService.java
@@ -0,0 +1,59 @@
+package com.jackpot.narratix.domain.service;
+
+import com.jackpot.narratix.domain.controller.response.ShareLinkActiveResponse;
+import com.jackpot.narratix.domain.entity.CoverLetter;
+import com.jackpot.narratix.domain.entity.ShareLink;
+import com.jackpot.narratix.domain.repository.CoverLetterRepository;
+import com.jackpot.narratix.domain.repository.ShareLinkRepository;
+import com.jackpot.narratix.global.exception.BaseException;
+import com.jackpot.narratix.global.exception.GlobalErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ShareLinkService {
+
+    private final CoverLetterRepository coverLetterRepository;
+    private final ShareLinkRepository shareLinkRepository;
+
+    @Transactional
+    public ShareLinkActiveResponse updateShareLinkStatus(String userId, Long coverLetterId, boolean active) {
+        validateCoverLetterOwnership(userId, coverLetterId);
+
+        if (active) {
+            return activateShareLink(coverLetterId);
+        } else {
+            return deactivateShareLink(coverLetterId);
+        }
+    }
+
+    private void validateCoverLetterOwnership(String userId, Long coverLetterId) {
+        CoverLetter coverLetter = coverLetterRepository.findByIdOrElseThrow(coverLetterId);
+        if (!coverLetter.isOwner(userId)) {
+            throw new BaseException(GlobalErrorCode.FORBIDDEN);
+        }
+    }
+
+    private ShareLinkActiveResponse activateShareLink(Long coverLetterId) {
+        ShareLink shareLink = shareLinkRepository.findById(coverLetterId)
+                .map(this::updateExistingShareLink)
+                .orElseGet(() -> createNewShareLink(coverLetterId));
+        return ShareLinkActiveResponse.activate(shareLink.getShareId());
+    }
+
+    private ShareLink updateExistingShareLink(ShareLink shareLink) {
+        shareLink.activate();
+        return shareLink;
+    }
+
+    private ShareLink createNewShareLink(Long coverLetterId) {
+        return shareLinkRepository.save(ShareLink.newActivatedShareLink(coverLetterId));
+    }
+
+    private ShareLinkActiveResponse deactivateShareLink(Long coverLetterId) {
+        shareLinkRepository.findById(coverLetterId).ifPresent(ShareLink::deactivate);
+        return ShareLinkActiveResponse.deactivate();
+    }
+}
