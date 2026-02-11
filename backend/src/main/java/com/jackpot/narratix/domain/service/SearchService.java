edc28d657b2b635e96a15eea16032c68d201ae82
livebylee <livebylee@gmail.com>
2026-02-11 14:12:11 +0900

    [feat] serviec 로직 구현 (#240)

--- backend/src/main/java/com/jackpot/narratix/domain/service/SearchService.java
+++ backend/src/main/java/com/jackpot/narratix/domain/service/SearchService.java
@@ -1,11 +1,14 @@
 package com.jackpot.narratix.domain.service;
 
 import com.jackpot.narratix.domain.controller.response.SearchCoverLetterResponse;
+import com.jackpot.narratix.domain.controller.response.SearchLibraryAndQnAResponse;
 import com.jackpot.narratix.domain.controller.response.SearchScrapResponse;
 import com.jackpot.narratix.domain.entity.CoverLetter;
 import com.jackpot.narratix.domain.entity.QnA;
+import com.jackpot.narratix.domain.entity.enums.QuestionCategoryType;
 import com.jackpot.narratix.domain.exception.SearchErrorCode;
 import com.jackpot.narratix.domain.repository.CoverLetterRepository;
+import com.jackpot.narratix.domain.repository.QnARepository;
 import com.jackpot.narratix.domain.repository.ScrapRepository;
 import com.jackpot.narratix.global.exception.BaseException;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +20,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class SearchService {
 
     private final ScrapRepository scrapRepository;
     private final CoverLetterRepository coverLetterRepository;
+    private final QnARepository qnARepository;
 
     public SearchScrapResponse searchScrap(
             String userId, String searchWord, Integer size, Long lastQnaId
@@ -67,6 +73,22 @@ public class SearchService {
         Page<CoverLetter> resultPage = coverLetterRepository.searchCoverLetters(userId, keyword, pageable);
 
         return SearchCoverLetterResponse.from(resultPage);
+    }
+
+    @Transactional(readOnly = true)
+    public SearchLibraryAndQnAResponse searchLibraryAndQnA(String userId, String searchWord, Integer size, Long lastQnAId) {
+        if (!StringUtils.hasText(searchWord)) { //공백이거나,null이면 오류 반환
+            throw new BaseException(SearchErrorCode.INVALID_SEARCH_KEYWORD);
+        }
+
+        String keyword = searchWord.trim();
+
+        List<QuestionCategoryType> questionLibraies = qnARepository.searchQuestionCategory(userId, keyword);
+
+        Slice<QnA> qnAs = qnARepository.searchQnA(userId, keyword, size, lastQnAId);
+
+        Long qnACount = qnARepository.countSearchQnA(userId, keyword);
+        return SearchLibraryAndQnAResponse.of(questionLibraies, qnACount, qnAs.getContent(), qnAs.hasNext());
     }
 
 }
