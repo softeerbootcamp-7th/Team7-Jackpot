e08c95054f23c6a369813e0406006a4b00b7f2a7
윤종근 <doodukly1999@gmail.com>
2026-02-11 15:34:21 +0900

    [refactor] 삼항 연산자를 활용하여 간결하게 함수 수정

--- frontend/src/features/upload/utils/generateAboutDate.ts
+++ frontend/src/features/upload/utils/generateAboutDate.ts
@@ -11,12 +11,7 @@ const today = new Date();
 
 export const yearList = generateYearList(today.getFullYear());
 
-const generateHalfInfo = (month: number) => {
-  let halfInfo = null;
-
-  if (month <= 6) halfInfo = 'first';
-  else halfInfo = 'second';
-  return halfInfo;
-};
+const generateHalfInfo = (month: number): 'first' | 'second' =>
+  month <= 6 ? 'first' : 'second';
 
 export const halfInfo = generateHalfInfo(today.getMonth() + 1);
