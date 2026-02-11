48aa0bcc38cce783f2caa8538da55e8c581c4e9b
livebylee <livebylee@gmail.com>
2026-02-12 00:18:21 +0900

    [feat] upload error code 정의 (#240)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/exception/UploadErrorCode.java
@@ -0,0 +1,17 @@
+package com.jackpot.narratix.domain.exception;
+
+import com.jackpot.narratix.global.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UploadErrorCode implements ErrorCode {
+
+    INVALID_CONTENT_TYPE_FOR_PDF(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "PDF만 업로드할 수 있습니다."),
+    FILE_SIZE_EXCEEDED(HttpStatus.valueOf(413), "파일 업로드 용량이 초과되었습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
