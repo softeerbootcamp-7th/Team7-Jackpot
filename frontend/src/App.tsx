ad138b87a96a04da4af990b6e4962767cb5e326e
using6843@gmail.com <using6843@gmail.com>
2026-02-11 17:38:09 +0900

    [feat] /cover-letter 진입 시 /cover-letter/list로 index 리다이렉트 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 
 import CoverLetterQnAFriendsPage from '@/pages//CoverLetterQnAFriendsPage';
 import CoverLetterLandingPage from '@/pages/CoverLetterLandingPage';
@@ -28,6 +28,10 @@ function App() {
             <Route path='/upload' element={<UploadPage />} />
             <Route path='/library' element={<LibraryPage />} />
             <Route path='/cover-letter' element={<CoverLetterLayout />}>
+              <Route
+                index
+                element={<Navigate to='/cover-letter/list' replace />}
+              />
               <Route
                 path='/cover-letter/list'
                 element={<CoverLetterLandingPage />}
