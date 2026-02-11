db7ce1f6b01e5104fb2906a58884c0b5c786794a
livebylee <livebylee@gmail.com>
2026-02-11 18:58:59 +0900

    [feat] Upload Api controller 구현 (#240)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/UploadController.java
@@ -0,0 +1,31 @@
+package com.jackpot.narratix.domain.controller;
+
+import com.jackpot.narratix.domain.controller.api.UploadApi;
+import com.jackpot.narratix.domain.controller.request.PresignedUrlRequest;
+import com.jackpot.narratix.domain.controller.response.PresignedUrlResponse;
+import com.jackpot.narratix.global.auth.UserId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/upload")
+public class UploadController implements UploadApi {
+
+    private final UploadService uploadService;
+
+    @Override
+    @GetMapping("/presignedurl")
+    public ResponseEntity<PresignedUrlResponse> createPresignedUrl(
+            @UserId String userId,
+            @RequestBody PresignedUrlRequest request
+
+    ) {
+        return ResponseEntity.ok(uploadService.createPresignedUrl(userId, request));
+    }
+
+}
