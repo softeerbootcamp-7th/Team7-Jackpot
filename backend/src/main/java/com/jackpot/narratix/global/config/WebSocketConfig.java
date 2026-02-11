a0ca55378ba1eaaaeb7b8264a71a5dde670f7ea2
kseysh <kseysh123@naver.com>
2026-02-11 16:42:03 +0900

    [feat] WebSocketConfig 추가 (#358)

---
+++ backend/src/main/java/com/jackpot/narratix/global/config/WebSocketConfig.java
@@ -0,0 +1,37 @@
+package com.jackpot.narratix.global.config;
+
+import com.jackpot.narratix.global.interceptor.StompHandshakeInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandshakeInterceptor stompHandshakeInterceptor;
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub"); // 클라이언트가 구독할 경로 (서버 -> 클라이언트)
+        // TODO: 추후 redis pub/sub 구조의 Message Broker를 사용해야 함
+        config.setApplicationDestinationPrefixes("/pub"); // 클라이언트가 보낼 경로 (클라이언트 -> 서버)
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/connect")
+//                .setAllowedOrigins(allowedOrigins)
+                .setAllowedOriginPatterns("*") // TODO: 배포 전 테스트를 위해 모든 Origin 허용
+                .addInterceptors(stompHandshakeInterceptor) // 링크 유효성 검사
+                .withSockJS();  // SockJS fallback 옵션 활성화 (웹소켓을 지원하지 않는 브라우저에 대한 대체 솔루션 제공)
+    }
+}
