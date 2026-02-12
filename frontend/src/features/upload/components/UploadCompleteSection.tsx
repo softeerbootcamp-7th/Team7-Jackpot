4e13dd90d11daea4a9405d31d4b6f3e82e153cf7
윤종근 <doodukly1999@gmail.com>
2026-02-13 01:00:10 +0900

    [feat] 자료 업로드 페이지 저장 완료 화면 구현

--- frontend/src/features/upload/components/UploadCompleteSection.tsx
+++ frontend/src/features/upload/components/UploadCompleteSection.tsx
@@ -1,24 +1,36 @@
+import { useNavigate } from 'react-router';
+
+import FolderIconInUpload from '@/assets/icons/FolderIconInUpload.png';
 import { UploadPageIcons as I } from '@/features/upload/icons';
 
 const UploadCompleteSection = () => {
+  // [윤종근] - TODO: 추후 useNavigate -> Link로 변경 시 수정
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div className='flex flex-col gap-1'>
-        <span className='text-title-m text-gray-600'>저장이 완료되었어요!</span>
-        <span className='text-body-l text-gray-400'>
-          총 3개의 문항이 라이브러리에 저장되었어요.
-        </span>
+    <div className='relative flex w-full items-center justify-center'>
+      <div className='absolute top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2'>
+        <I.UploadCompleteBackground />
       </div>
-      <div>그림 영역</div>
-      <div className='text-title-s flex gap-3'>
-        <button className='flex gap-2 rounded-lg bg-gray-50 px-5 py-3'>
-          <I.FolderIconInButton />
-          <span className='text-gray-600'>새로운 자료 업로드하기</span>
-        </button>
-        <button className='flex gap-2 rounded-lg bg-gray-900 px-5 py-3'>
-          <I.FolderIconInButton />
-          <span className='text-white'>라이브러리에서 보기</span>
-        </button>
+      <div className='z-10 mt-24 flex flex-col items-center gap-12'>
+        <img src={FolderIconInUpload} />
+
+        <div className='text-title-s flex gap-3'>
+          <button
+            onClick={() => navigate('/upload')}
+            className='flex cursor-pointer gap-2 rounded-lg bg-gray-50 px-5 py-3'
+          >
+            <I.UploadIconInButton />
+            <span className='text-gray-600'>새로운 자료 업로드하기</span>
+          </button>
+          <button
+            onClick={() => navigate('/library')}
+            className='flex cursor-pointer gap-2 rounded-lg bg-gray-900 px-5 py-3'
+          >
+            <I.FolderIconInButton />
+            <span className='text-white'>라이브러리에서 보기</span>
+          </button>
+        </div>
       </div>
     </div>
   );
