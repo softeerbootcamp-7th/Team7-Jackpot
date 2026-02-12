8bee4d4f3251b18fda5204750dd715581493579b
Sminp <0102262@gmail.com>
2026-02-12 09:30:01 +0900

    [feat] 중첩 라우팅을 이용하여 library의 하위 LNB를 URL로 표시할 수 있도록 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import CoverLetterLandingPage from '@/pages/CoverLetterLandingPage';
 import CoverLetterPage from '@/pages/CoverLetterPage';
 import HomePage from '@/pages/HomePage';
-import LibraryPage from '@/pages/LibraryPage';
 import LoginPage from '@/pages/LoginPage';
 import ReviewPage from '@/pages/ReviewPage';
 import SignUpPage from '@/pages/SignUpPage';
 import UploadPage from '@/pages/UploadPage';
 
+import DetailView from '@/features/library/components/DetailView';
+import LibraryLayout from '@/features/library/components/LibraryLayout';
+import LibrarySidebarLayout from '@/features/library/components/LibrarySidebarLayout';
+import { emptyCaseText } from '@/features/library/constants';
+import EmptyCase from '@/shared/components/EmptyCase';
 import RootLayout from '@/shared/components/RootLayout';
 
 function App() {
@@ -23,8 +27,35 @@ function App() {
           <Route path='/upload' element={<UploadPage />} />
           <Route path='/cover-letter' element={<CoverLetterLandingPage />} />
           <Route path='/cover-letter/new' element={<CoverLetterPage />} />
-          <Route path='/library' element={<LibraryPage />} />
-          {/* <Route path='/library?id:' element={<LibraryPage />} /> */}
+          <Route path='/library' element={<LibraryLayout />}>
+            <Route element={<LibrarySidebarLayout />}>
+              <Route path='company'>
+                <Route
+                  index
+                  element={<EmptyCase {...emptyCaseText.folder} />}
+                />
+                <Route
+                  path=':companyName'
+                  element={<EmptyCase {...emptyCaseText.folder} />}
+                />
+                <Route
+                  path=':companyName/:coverLetterId'
+                  element={<DetailView />}
+                />
+              </Route>
+              <Route path='qna'>
+                <Route
+                  index
+                  element={<EmptyCase {...emptyCaseText.folder} />}
+                />
+                <Route
+                  path=':qnAName'
+                  element={<EmptyCase {...emptyCaseText.folder} />}
+                />
+                <Route path=':qnAName/:qnAId' element={<DetailView />} />
+              </Route>
+            </Route>
+          </Route>
           <Route path='/coverLetter' element={<CoverLetterPage />} />
           <Route path='/review/:id' element={<ReviewPage />} />
           {/* <Route path="/recruit" element={<RecruitPage />}/> */}
