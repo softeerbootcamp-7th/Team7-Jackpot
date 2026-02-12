efe09b535d4138c92962c564312215d464cabc1b
using6843@gmail.com <using6843@gmail.com>
2026-02-12 17:18:24 +0900

    [chore] icon 파일들 추가

---
+++ frontend/src/features/coverLetter/icons/FileDocument.tsx
@@ -0,0 +1,39 @@
+const FileDocument = () => {
+  return (
+    <svg
+      width='28'
+      height='28'
+      viewBox='0 0 28 28'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M16.4463 2.33936C16.7133 2.36591 16.9649 2.48393 17.1562 2.67529L22.9893 8.50928C23.2078 8.72795 23.3309 9.02436 23.3311 9.3335V22.1675C23.3307 24.1001 21.7637 25.6673 19.8311 25.6675H8.16406C6.23155 25.6671 4.66437 24.1 4.66406 22.1675V3.50049C4.66411 2.8563 5.1869 2.33367 5.83105 2.3335H16.3311L16.4463 2.33936ZM16.332 9.3335H20.5146L16.332 5.15088V9.3335Z'
+        fill='#AEB7F3'
+      />
+      <path
+        d='M10.5 15.3325H17.5'
+        stroke='#7371E3'
+        strokeWidth='2.33333'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M10.5 19.9995H17.5'
+        stroke='#7371E3'
+        strokeWidth='2.33333'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M16.7344 9.112V3.32324L22.5231 9.112H16.7344Z'
+        fill='#7371E3'
+        stroke='#7371E3'
+        strokeWidth='1.4'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default FileDocument;
---
+++ frontend/src/features/coverLetter/icons/ReviewMessageIcon.tsx
@@ -0,0 +1,29 @@
+import { type SVGProps } from 'react';
+
+export const ReviewMessageIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='20'
+      fill='none'
+      viewBox='0 0 20 20'
+      {...props}
+    >
+      <path
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.5'
+        d='M17.5 12.5a1.667 1.667 0 0 1-1.667 1.667h-10L2.5 17.5v-13.333A1.667 1.667 0 0 1 4.167 2.5h11.666A1.667 1.667 0 0 1 17.5 4.167v8.333z'
+      />
+      <path
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.5'
+        d='M6 7.5h8M6 11h5'
+      />
+    </svg>
+  );
+};
