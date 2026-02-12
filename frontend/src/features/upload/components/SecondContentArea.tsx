cf560b1de04d36b3925d99c674d58674a97cd6e6
윤종근 <doodukly1999@gmail.com>
2026-02-12 22:10:47 +0900

    [refactor] 중첩 라우팅 적용을 위해 쿼리 파라미터 이용 방식으로 변경

--- frontend/src/features/upload/components/SecondContentArea.tsx
+++ frontend/src/features/upload/components/SecondContentArea.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router';
 
 import SecondContentAreaHeader from '@/features/upload/components/SecondContentAreaHeader';
 import SecondContentItem from '@/features/upload/components/SecondContentItem';
 
-interface SecondContentAreaProps {
-  nextStep?: (step: string) => void;
-}
-const SecondContentArea = ({ nextStep }: SecondContentAreaProps) => {
-  const [tabState, setTabState] = useState<1 | 2 | 3>(1);
+const SecondContentArea = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const coverLetterParam = searchParams.get('coverLetterId');
+  const currentId: number =
+    coverLetterParam && ['1', '2', '3'].includes(coverLetterParam)
+      ? Number(coverLetterParam)
+      : 1;
+
+  const hanldeNextStep = () => {
+    navigate('/upload/complete', { replace: true });
+  };
+  const handleCoverLetterIdChange = (newId: number) => {
+    setSearchParams({ coverLetterId: newId.toString() }, { replace: true });
+  };
   return (
-    <>
-      <SecondContentAreaHeader nextStep={nextStep} />
-      <SecondContentItem tabState={tabState} setTabState={setTabState} />
-    </>
+    <div className='flex flex-col gap-6'>
+      <SecondContentAreaHeader nextStep={hanldeNextStep} />
+      <SecondContentItem
+        tabState={currentId}
+        setTabState={handleCoverLetterIdChange}
+      />
+    </div>
   );
 };
 
