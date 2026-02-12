1747922775abcdad0f7e9d05afab040c084f1190
윤종근 <doodukly1999@gmail.com>
2026-02-12 22:11:08 +0900

    [refactor] lint 적용

--- frontend/src/features/upload/components/SecondContentAreaHeader.tsx
+++ frontend/src/features/upload/components/SecondContentAreaHeader.tsx
@@ -1,22 +1,28 @@
 import { UploadPageIcons as I } from '@/features/upload/icons';
 
 interface SecondContentAreaHeaderProps {
-    nextStep?: (step: string) => void;
+  nextStep?: (step: string) => void;
 }
 
-const SecondContentAreaHeader = ({nextStep}: SecondContentAreaHeaderProps) => {
+const SecondContentAreaHeader = ({
+  nextStep,
+}: SecondContentAreaHeaderProps) => {
   return (
     <div>
-      <div className='flex justify-between items-center select-none'>
+      <div className='flex items-center justify-between select-none'>
         <div className='flex gap-[0.625rem]'>
           <I.AILabelingIcon size='32' />
-          <div className='font-bold text-2xl'>
+          <div className='text-2xl font-bold'>
             <span className='text-purple-500'>두 번째</span> 자기소개서는 총
             <span className='text-purple-500'> 3개</span>의 문항으로
             분류되었어요!
           </div>
         </div>
-        <button type='button' onClick={() => nextStep?.('3')} className='px-5 py-3 rounded-lg bg-gray-900 text-white font-bold text-lg cursor-pointer'>
+        <button
+          type='button'
+          onClick={() => nextStep?.('3')}
+          className='cursor-pointer rounded-lg bg-gray-900 px-5 py-3 text-lg font-bold text-white'
+        >
           저장하기
         </button>
       </div>
