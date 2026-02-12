4da9db3373c243b38d33bfb492ed3247fceaced6
Sminp <0102262@gmail.com>
2026-02-12 17:34:07 +0900

    [feat] 사용성을 극대화하고자 중첩 라우팅 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 
 import CoverLetterLandingPage from '@/pages/CoverLetterLandingPage';
 import CoverLetterPage from '@/pages/CoverLetterPage';
@@ -28,6 +28,9 @@ function App() {
           <Route path='/cover-letter' element={<CoverLetterLandingPage />} />
           <Route path='/cover-letter/new' element={<CoverLetterPage />} />
           <Route path='/library' element={<LibraryLayout />}>
+          {/* [박소민] /library로 접속시 자동으로 /library/company로 이동 */}
+          {/* TODO: 렌더링을 최소화할 수 있는 방법이 없는지 확인 (라우팅 변경해도 됨) */}
+            <Route index element={<Navigate to='/library/company' replace />} />
             <Route element={<LibrarySidebarLayout />}>
               <Route path='company'>
                 <Route
