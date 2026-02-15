8e32aa6332564dd9d8ce603f0e0acb27771abc9e
윤종근 <doodukly1999@gmail.com>
2026-02-16 00:18:27 +0900

    [feat] SSE RootLayout에서 연결

--- frontend/src/shared/components/RootLayout.tsx
+++ frontend/src/shared/components/RootLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router';
 
 import PageGlobalHeader from '@/shared/components/PageGlobalHeader';
+import { useSSE } from '@/shared/hooks/useSSE';
 
 const RootLayout = () => {
+  useSSE();
+
   return (
     <div>
       <PageGlobalHeader />
