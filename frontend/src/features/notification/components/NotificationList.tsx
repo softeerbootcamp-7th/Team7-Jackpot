133f613e022f3714d9a4adeecee23632f4262b06
윤종근 <doodukly1999@gmail.com>
2026-02-15 09:45:31 +0900

    [feat] 알림 드롭다운 무한스크롤 기능 API 연결

--- frontend/src/features/notification/components/NotificationList.tsx
+++ frontend/src/features/notification/components/NotificationList.tsx
@@ -1,26 +1,52 @@
 import NotificationItem from '@/features/notification/components/NotificationItem';
+import {
+  useGetAllNotification,
+  useReadEachNotification,
+} from '@/features/notification/hooks/useNotification';
+import { useScrollTouchEndObserver } from '@/features/notification/hooks/useScrollTouchEndObserver';
 import type { NotificationType } from '@/features/notification/types/notification';
 
 interface NotificationListProps {
   handleDropdown: (isOpen: boolean) => void;
-  data: NotificationType[];
 }
 
-const NotificationList = ({ handleDropdown, data }: NotificationListProps) => {
+const NotificationList = ({ handleDropdown }: NotificationListProps) => {
+  const {
+    data: notificationListData,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useGetAllNotification();
+  const { mutateAsync: readEachNotification } = useReadEachNotification();
+  const { targetRef } = useScrollTouchEndObserver({
+    enabled: !!hasNextPage && !isFetchingNextPage,
+    onIntersect: () => fetchNextPage(),
+  });
+
+  if (!notificationListData) return null;
+
   return (
     <>
-      {data.map((each, index) => (
-        <button
-          type='button'
-          onClick={() => {
-            handleDropdown(false);
-          }}
-          key={index}
-          className='w-full cursor-pointer rounded-md py-[0.875rem] text-left text-[0.813rem] font-medium text-gray-700 hover:bg-gray-50'
-        >
-          <NotificationItem data={each} />
-        </button>
+      {notificationListData.pages.map((page, pageIndex) => (
+        <div key={pageIndex}>
+          {page.notifications.map((each: NotificationType) => (
+            <button
+              type='button'
+              onClick={() => {
+                readEachNotification(Number(each.id));
+                handleDropdown(false);
+              }}
+              key={each.id}
+              className='w-full cursor-pointer rounded-md py-[0.875rem] text-left text-[0.813rem] font-medium text-gray-700 hover:bg-gray-50'
+            >
+              <NotificationItem data={each} />
+            </button>
+          ))}
+        </div>
       ))}
+      <div ref={targetRef} className='flex h-4 items-center justify-center'>
+        {isFetchingNextPage && <span>로딩 중...</span>}
+      </div>
     </>
   );
 };
