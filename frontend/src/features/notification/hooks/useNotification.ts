a7c089542fd237e12b32da9e08b97d999bb625a3
윤종근 <doodukly1999@gmail.com>
2026-02-15 09:44:49 +0900

    [feat] tanstack query를 이용한 알림 드롭다운 커스텀 훅 제작

---
+++ frontend/src/features/notification/hooks/useNotification.ts
@@ -0,0 +1,97 @@
+import {
+  type InfiniteData,
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+
+import type {
+  NotificationResponse,
+  NotificationType,
+} from '../types/notification';
+
+import { getAccessToken } from '@/features/auth/libs/tokenStore';
+import { apiClient } from '@/shared/api/apiClient';
+
+// 알림 목록을 조회하는 커스텀 훅
+export const useGetAllNotification = () => {
+  return useInfiniteQuery({
+    queryKey: ['notificationList'],
+    queryFn: ({ pageParam }) =>
+      apiClient.get({
+        endpoint: '/notification/all',
+        params: {
+          size: 10,
+          lastNotificationId: pageParam ?? undefined,
+        },
+      }),
+    initialPageParam: null,
+    getNextPageParam: (lastPage) => {
+      if (!lastPage.hasNext) return undefined;
+      const notificationList = lastPage.notifications;
+      return notificationList.length > 0
+        ? notificationList[notificationList.lenght - 1].id
+        : undefined;
+    },
+    staleTime: 0,
+    enabled: !!getAccessToken(),
+  });
+};
+
+// 읽지 않은 알림 개수를 조회하는 커스텀 훅
+export const useGetNotificationCount = () => {
+  return useQuery({
+    queryKey: ['notificationCount'],
+    queryFn: () => apiClient.get({ endpoint: '/notification/count' }),
+    staleTime: 0,
+    enabled: !!getAccessToken(),
+  });
+};
+
+// 단건 알림 읽음 처리 커스텀 훅
+export const useReadEachNotification = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (notificationId: number) =>
+      apiClient.patch({ endpoint: `/notification/${notificationId}/read` }),
+    onSuccess: (_, notificationId) => {
+      queryClient.setQueryData<InfiniteData<NotificationResponse>>(
+        ['notificationList'],
+        (oldData) => {
+          if (!oldData) return oldData;
+
+          return {
+            ...oldData,
+            pages: oldData.pages.map((page) => ({
+              ...page,
+              notifications: page.notifications.map(
+                (notification: NotificationType) =>
+                  notification.id === notificationId
+                    ? { ...notification, isRead: true }
+                    : notification,
+              ),
+            })),
+          };
+        },
+      );
+      queryClient.invalidateQueries({ queryKey: ['notificationCount'] });
+    },
+    // [윤종근] - TODO: 토스트 메시지로 수정 필요
+    onError: (error) => console.error('에러입니다.', error),
+  });
+};
+
+// 모든 알림 읽음 처리 커스텀 훅
+export const useReadAllNotification = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: () => apiClient.patch({ endpoint: '/notification/all/read' }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notificationList'] });
+      queryClient.invalidateQueries({ queryKey: ['notificationCount'] });
+    },
+    // [윤종근] - TODO: 토스트 메시지로 수정 필요
+    onError: (error) => console.error('에러입니다.', error),
+  });
+};
