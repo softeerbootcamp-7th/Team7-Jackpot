76f58efc4737f77ddb45bb640b1b821df403767f
윤종근 <doodukly1999@gmail.com>
2026-02-16 22:03:25 +0900

    [feat] SSE 재연결 로직 수정

--- frontend/src/shared/hooks/useSSE.ts
+++ frontend/src/shared/hooks/useSSE.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { useQueryClient } from '@tanstack/react-query';
 
@@ -11,6 +11,7 @@ import type { SSEPayload } from '@/shared/types/sse';
 export const useSSE = () => {
   const token = getAccessToken();
   const queryClient = useQueryClient();
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   useEffect(() => {
     if (!token) return;
@@ -41,22 +42,62 @@ export const useSSE = () => {
             );
           }
         });
-      } catch (error) {
-        if (error instanceof Error) {
-          if (error.name !== 'AbortError') {
-            console.error('SSE 연결 끊김, 3초 후 재연결...', error);
-            setTimeout(connectSSE, 3000);
-          } else {
-            console.error('알수없는 오류 발생');
-          }
+
+        // 정상 종료 시에도 즉시 재연결하지 않고 1초 대기
+        if (!controller.signal.aborted) {
+          console.log('SSE 정상 종료. 소켓 정리 후 1초 뒤 재연결');
+
+          if (timerRef.current) clearTimeout(timerRef.current);
+
+          timerRef.current = setTimeout(() => {
+            connectSSE();
+          }, 1000);
         }
+      } catch (error) {
+        // if (error instanceof Error) {
+        //   if (error.name !== 'AbortError') {
+        //     console.error('SSE 연결 끊김, 3초 후 재연결...', error);
+        //     setTimeout(connectSSE, 3000);
+        //   } else {
+        //     console.error('알수없는 오류 발생');
+        //   }
+        // }
+
+        // 400 에러 (한 유저 당 최대 SSE 연결 개수 5개 초과 오류) 발생 시 재연결 안함
+        if (error instanceof Error && error.message.includes('400')) {
+          console.error('연결 제한 초과. 재연결 중단.');
+          return;
+        }
+        // AbortError는 사용자가 의도적으로 끊은 것이므로 재연결 안함
+        if (error instanceof Error && error.name === 'AbortError') {
+          return;
+        }
+        // 기존 타이머 제거 후 새로 설정
+        console.error('SSE 에러, 3초 후 재연결...', error);
+        if (timerRef.current) clearTimeout(timerRef.current);
+
+        timerRef.current = setTimeout(() => {
+          // 3초가 지난 시점에 이미 aborted 되었다면 재연결 하지 않음
+          if (!controller.signal.aborted) {
+            connectSSE();
+          }
+        }, 3000);
       }
     };
 
     connectSSE();
 
-    return () => {
+    // cleanup 함수
+    const handleBeforeUnload = () => {
       controller.abort();
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+      controller.abort();
+      // 대기 중인 재연결 취소
+      if (timerRef.current) clearTimeout(timerRef.current);
     };
   }, [token, queryClient]);
 };
