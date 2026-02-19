71a08fcc28e9fae4c89e4feb692f578b72ce320e
using6843@gmail.com <using6843@gmail.com>
2026-02-19 21:03:46 +0900

    [chore] 코드래빗 리뷰 반영

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -6,10 +6,10 @@ const NotFoundPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className='flex h-screen w-full max-w-full flex-col items-center justify-center bg-white'>
-      <div className='relative h-[35rem] max-w-full'>
+    <main className='flex h-screen w-full max-w-full flex-col items-center justify-center bg-white'>
+      <div className='relative aspect-[810/424] w-full max-w-[1000px]'>
         <NotFoundIllustration className='h-full w-full' aria-hidden='true' />
-        <div className='absolute top-1/4 left-1/2 inline-flex w-96 -translate-x-1/2 flex-col items-center justify-start gap-3'>
+        <div className='absolute top-1/4 left-1/2 inline-flex w-96 max-w-full -translate-x-1/2 flex-col items-center justify-start gap-3'>
           <h1 className='self-stretch text-center text-2xl font-bold text-gray-950'>
             페이지를 찾을 수 없습니다.
           </h1>
@@ -27,7 +27,7 @@ const NotFoundPage = () => {
       >
         홈으로 돌아가기
       </button>
-    </div>
+    </main>
   );
 };
 
