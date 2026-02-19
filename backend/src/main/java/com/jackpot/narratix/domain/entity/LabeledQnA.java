bbfb538a83e7eb91de6b77ff4c6a47a1cfe63430
livebylee <livebylee@gmail.com>
2026-02-19 14:12:05 +0900

    [feat] labeled_qna 엔티티 추가 (#442)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/LabeledQnA.java
@@ -0,0 +1,42 @@
+package com.jackpot.narratix.domain.entity;
+
+import com.jackpot.narratix.domain.entity.enums.QuestionCategoryType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "labeled_qna")
+public class LabeledQnA {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "upload_file_id")
+    private UploadFile uploadFile;
+
+    @Column(columnDefinition = "TEXT")
+    private String question;
+
+    @Column(columnDefinition = "TEXT")
+    private String answer;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "question_category", nullable = false)
+    private QuestionCategoryType questionCategory;
+
+    @Builder
+    public LabeledQnA(UploadFile uploadFile, String question, String answer, QuestionCategoryType category) {
+        this.uploadFile = uploadFile;
+        this.question = question;
+        this.answer = answer;
+        this.questionCategory = category;
+    }
+
+    
+}
