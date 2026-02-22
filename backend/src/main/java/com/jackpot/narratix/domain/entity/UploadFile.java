07e1217707c5bddf125c6a17105e01b1d18cc0c0
kseysh <kseysh123@naver.com>
2026-02-22 15:11:26 +0900

    [refactor] 엔티티 cascade 설정 추가 (#495)

--- backend/src/main/java/com/jackpot/narratix/domain/entity/UploadFile.java
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/UploadFile.java
@@ -6,6 +6,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -19,8 +24,11 @@ public class UploadFile {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "upload_job_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private UploadJob uploadJob;
 
+    @OneToMany(mappedBy = "uploadFile", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LabeledQnA> labeledQnAs = new ArrayList<>();
 
     @Column(nullable = false)
     private String s3Key;
@@ -51,10 +59,11 @@ public class UploadFile {
     }
 
     @Builder
-    public UploadFile(String id, String originalFileName, String s3Key) {
+    public UploadFile(String id, String s3Key) {
         this.id = id;
         this.s3Key = s3Key;
         this.status = UploadStatus.PENDING;
+        this.labeledQnAs = new ArrayList<>();
     }
 
     protected void assignToJob(UploadJob uploadJob) {
